f4db528c4f49684babf16889c048aa6492a668bc
‘Yonatan <yonatan@yaltman.com>
2020-07-22 10:41:44 +0300

    post

--- src/office/login/login.component.ts
+++ src/office/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../service/user.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +11,7 @@ import { UserService } from '../service/user.service';
 export class LoginComponent implements OnInit {
   username;
   password;
+  _currentName;
   label = {
     username: 'שם משתמש',
     password: 'סיסמה'
@@ -16,9 +19,59 @@ export class LoginComponent implements OnInit {
   btn = {
     submit: 'התחבר',
   };
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {
+    console.log('constructor', new Date());
+
+  }
+
+
+  names$ = new Observable<string[]>(observer => {
+    try {
+      setTimeout(() => {
+        console.log('Someone called me!');
+
+        observer.next(['yonatan', 'harry', 'maayan']);
+
+        observer.complete();
+      }, 1000 * 3);
+
+    } catch (error) {
+      observer.error(error);
+    }
+  });
+
+  firstname$ = this.names$.pipe(
+    map(list => list[0])
+  );
+  names2$ = this.names$;
+
 
   ngOnInit() {
+    console.log('ngOnInit', new Date());
+    this.getData();
+  }
+
+  getData() {
+    this.names$.subscribe(names => {
+      console.log('typeof names', typeof names);
+      console.log('subscribe names$', names);
+
+    }, error => {
+      console.log(error);
+
+    });
+    this.names2$.subscribe(names2 => {
+      console.log('typeof names2', typeof names2);
+      console.log('subscribe names2$', names2);
+
+    });
+    this.firstname$.subscribe(nameFromObservable => {
+      console.log('typeof name', typeof nameFromObservable);
+      console.log('subscribe firstname$', nameFromObservable);
+      this._currentName = nameFromObservable;
+    });
+    console.log('after all subscribes');
+
   }
   submit1() {
     const { username, password } = this;
@@ -38,10 +91,19 @@ export class LoginComponent implements OnInit {
   }
   submit(user) {
     console.log(user);
-    this.userService.login(user).subscribe(res => {
-      console.log('Response From Server', res);
+    const req = this.userService.login(user);
+    req.subscribe(
+      res => {
+        console.log('Response From Server', res);
 
-    });
+      },
+      error => {
+        console.error(error);
+
+      },
+      () => {
+        // complete
+      });
 
   }
 
--- src/office/service/user.service.ts
+++ src/office/service/user.service.ts
@@ -8,11 +8,29 @@ const BASE_URL = 'http://localhost:54867/api/';
   providedIn: 'root'
 })
 export class UserService {
+  post = new Observable<any>(observer => {
+    try {
 
+      const result = goToServer();
+
+      observer.next(result);
+      observer.complete();
+
+    } catch (error) {
+      observer.error(error);
+    }
+
+
+  });
   constructor(private http: HttpClient) { }
 
   login(user: any): Observable<any> {
     const url = BASE_URL + 'user';
     return this.http.post(url, user);
   }
+
+
+}
+function goToServer() {
+  return '';
 }

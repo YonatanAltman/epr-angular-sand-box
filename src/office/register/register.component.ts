10aca4ce65f1e1dbe46069fa0755065d3b6a4702
‘Yonatan <yonatan@yaltman.com>
2020-07-26 19:07:43 +0300

    form control!

--- src/office/register/register.component.ts
+++ src/office/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MyValidators } from './input.validators';
 
 @Component({
   selector: 'app-register',
@@ -7,17 +8,48 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  currentName: string;
   label = {
     name: 'First Name',
+    firstname: 'First Name',
+    lastname: 'Last Name',
+    password: 'Password',
+    phone: 'Phone',
     email: 'Email',
+  };
+  btn = {
+    ok: 'legister',
+    cancel: 'cancel',
+  };
+  controls = {
+    firstname: 'firstname',
+    lastname: 'lastname',
+    password: 'password',
+    phone: 'phone',
+    email: 'email',
   }
   nameControl = new FormControl();
   emailControl = new FormControl();
+
+  rForm = new FormGroup({
+    firstname: new FormControl('', Validators.required),
+    lastname: new FormControl(undefined, [Validators.minLength(2), Validators.maxLength(10)]),
+    password: new FormControl('', MyValidators.ValidatePassword),
+    phone: new FormControl('', MyValidators.ValidatePhone),
+    email: new FormControl(undefined, Validators.email)
+  });
   constructor() { }
 
   ngOnInit() {
   }
   print() {
+    console.log(this.rForm.value);
+
+  }
+  cancel() {
+    this.rForm.reset();
+  }
+  print1() {
     console.log(this.nameControl.value);
     console.log(this.emailControl.value);
 

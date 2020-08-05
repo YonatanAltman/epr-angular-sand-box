e908993c02614cc86417dfaf00a4dc5f22c352bb
‘Yonatan <yonatan@yaltman.com>
2020-08-05 10:46:37 +0300

    dfasfdaf

--- src/input/input.model.ts
+++ src/input/input.model.ts
@@ -1,13 +1,18 @@
 import { ValidatorFn } from '@angular/forms';
 
-export type TInput = 'password' | 'text' | 'email' | 'date' | 'buttonGroup';
-
+export type TInput = 'password' | 'text'| 'select' | 'email' | 'date' | 'buttonGroup';
+export interface IListItem {
+    label: any;
+    value: any;
+    color?: string;
+}
 export interface IInputConfig {
-    label: string;
+    label?: string;
     type: TInput;
     formControlName?: string;
     placeholder?: string;
     error?: string;
     value?: any;
     validators?: ValidatorFn[];
+    list: IListItem[];
 }
